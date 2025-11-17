12b74f141f8cefab446635753bc0f8c1d979ba54
Your Name <you@example.com>
2025-11-17 15:22:43 +0530

    added dot product

---
+++ dotproduct.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the size of vectors: ";
+    cin >> n;
+
+    vector<int> A(n), B(n);
+    cout << "Enter elements of first vector:\n";
+    for (int i = 0; i < n; ++i)
+        cin >> A[i];
+
+    cout << "Enter elements of second vector:\n";
+    for (int i = 0; i < n; ++i)
+        cin >> B[i];
+
+    int dotProduct = 0;
+    for (int i = 0; i < n; ++i)
+        dotProduct += A[i] * B[i];
+
+    cout << "Dot Product = " << dotProduct << endl;
+
+    return 0;
+}
+// changes made by nice dhillon
+// there is an error
